d2b2928929156faf3e4c9ed9e1b4bdeaa5c03992
Joseph765 <josephchauvin@Mac.lan>
2025-06-24 14:25:28 -0400

    removed meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,10 +14,6 @@
     <meta name="keywords" content="Video Game Music Composer">
     <meta name="author" content="Joseph Chauvin">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta property="og:image" content="http://josephchauvin.com/me-in-the-studio.jpg"> -->
-    <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="1024">
-    <meta property="og:image:height" content="1024">
 </svelte:head>
 
 <div class="layout">
