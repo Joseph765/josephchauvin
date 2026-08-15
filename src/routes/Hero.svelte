a8c35f8c62615ab2fabcbb7c540a574094789782
Joseph765 <josephchauvin@Josephs-MacBook-Air.local>
2026-08-15 18:18:10 -0500

    fixed padding on hero youtube iframe

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -6,7 +6,7 @@
 <section id="hero">
     <Container size="xl">
         <Flex direction="column" gap="l" align="center">
-            <iframe style="display: block; max-width: 1000px; width: calc(100vw - 2rem); height: auto; aspect-ratio: 16/9; padding-inline: 0rem;" src="https://www.youtube.com/embed/KvZztfj-aA4?si=mCjWf5R-va-2KVx-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe style="display: block; max-width: 1000px; width: calc(100vw - 4rem); height: auto; aspect-ratio: 16/9; padding-inline: 0rem;" src="https://www.youtube.com/embed/KvZztfj-aA4?si=mCjWf5R-va-2KVx-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <Text align="center">Hello! I'm Joseph, a video game sound designer. I create immersive and satisfying sound effects that bring games to life.</Text>
         </Flex>
     </Container>
